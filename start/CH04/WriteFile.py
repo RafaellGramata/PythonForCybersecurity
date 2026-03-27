4ac4430ab91e591468a7c9204ec6ffa1cfae0ede
RafaellGramata <rafaellgramata01@gmail.com>
2026-03-26 21:52:41 -0700

    CH 4 writefile update

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
-# By 
+# By Rafaell
+
+# Open file for writing. Change "w" to "a" to append instead.
+test_file = open("testfile.txt", "w")
+
+# Write lines to the file
+test_file.write("Hello World\n")
+test_file.write("My name is Rafaell\n")
+test_file.write("I like rubber ducks\n")
+
+# Close the file
+test_file.close()
