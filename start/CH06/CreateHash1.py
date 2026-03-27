9158279f2f57f90a9b0e9e0cdfbe47dfa6911957
RafaellGramata <rafaellgramata01@gmail.com>
2026-03-27 10:07:51 -0700

    CH 6 hash updates

--- start/CH06/CreateHash1.py
+++ start/CH06/CreateHash1.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # Script that hashes a password
-# By 
+# By Rafaell
+
+# Import Python modules
+import crypt
+
+# Prompt user for plain-text password
+plain_pass = input("What is the password? ")
+
+# Print out hashes
+print("MD5 : {0}".format(crypt.crypt(plain_pass,"$1$")))
+print("Blowfish : {0}".format(crypt.crypt(plain_pass,"$2a$")))
+print("eksblofish: {0}".format(crypt.crypt(plain_pass,"$2y$")))
+print("SHA-256 : {0}".format(crypt.crypt(plain_pass,"$5$")))
+print("SHA-512 : {0}".format(crypt.crypt(plain_pass,"$6$")))
